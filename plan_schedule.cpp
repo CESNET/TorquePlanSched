df5deba8805cd1df77c792af0f8ec7cf726b9e7f
vchlum <chlumskyvaclav@gmail.com>
2016-06-08 10:04:05 +0200

    cleanup

--- plan_schedule.cpp
+++ plan_schedule.cpp
@@ -229,14 +229,14 @@ badval:
   }  /* END decode_time() */
 
 bool is_prefix(string const& s1, string const&s2)
-{
-    const char*p = s1.c_str();
-    const char*q = s2.c_str();
-    while (*p&&*q)
-        if (*p++!=*q++)
-            return false;
-    return true;
-}
+  {
+  const char*p = s1.c_str();
+  const char*q = s2.c_str();
+  while (*p&&*q)
+    if (*p++!=*q++)
+      return false;
+  return true;
+  }
 
 std::set<std::string> filter_exclude(std::set<std::string> strings, bool select_exclude)
   {
@@ -244,14 +244,16 @@ std::set<std::string> filter_exclude(std::set<std::string> strings, bool select_
   std::set<std::string>::const_iterator it;
 
   for (it = strings.begin(); it != strings.end(); it++)
-	  if (select_exclude) {
-	  if (is_prefix(*it,"min_") || is_prefix(*it,"max_"))
-	     output.insert(*it);
-	  } else
-	  {
-	  if (!is_prefix(*it,"min_") && !is_prefix(*it,"max_"))
-	     output.insert(*it);
-	  }
+    if (select_exclude)
+      {
+      if (is_prefix(*it,"min_") || is_prefix(*it,"max_"))
+        output.insert(*it);
+      }
+    else
+      {
+      if (!is_prefix(*it,"min_") && !is_prefix(*it,"max_"))
+        output.insert(*it);
+      }
 
   return output;
   }
@@ -391,11 +393,11 @@ int find_property(char **properties,char *property)
   {
   while (*properties != NULL)
     {
-	if (strcmp(*properties, property) == 0)
-	  return 0;
-
-	properties++;
-	}
+    if (strcmp(*properties, property) == 0)
+      return 0;
+    
+    properties++;
+    }
 
   /* property not found */
   return 1;
@@ -413,29 +415,30 @@ int check_cluster_suitable(server_info* p_info, plan_cluster* cl, plan_job* job)
   bool isbackfill = false;
   
   if (job->req_scratch_local == -2)
-      return 1;
-
-  if (is_prefix(job->jinfo->queue->name,"backfill") and cl -> cluster_properties.find("q_backfill") != cl -> cluster_properties.end() )
-	  isbackfill = true;
-
-  if (!isbackfill) {
-  if (job->jinfo->queue->required_property != NULL && !is_prefix(string(job->jinfo->queue->required_property),"q_")
-		  && cl -> cluster_properties.find(string(job->jinfo->queue->required_property)) == cl -> cluster_properties.end())
     return 1;
 
-  if (strcmp(job->jinfo->queue->name, conf.default_queue) == 0 || is_prefix(job->jinfo->queue->name,"q_") || is_prefix(job->jinfo->queue->name,"backfill"))
-	for (int j = 0; j < p_info->num_queues; ++j)
-	  if (p_info->queues[j]->required_property != NULL && strlen(p_info->queues[j]->required_property) > 0)
-		if (!is_prefix(p_info->queues[j]->required_property,"q_")
-				&& cl -> cluster_properties.find(string(p_info->queues[j]->required_property)) != cl -> cluster_properties.end())
-		{
-          /* pokud na clusteru tato fronta explicitne je tak to nezakazujem, muze tam*/
-		  if (strcmp(job->jinfo->queue->name, p_info->queues[j]->name) == 0)
-			  continue;
+  if (is_prefix(job->jinfo->queue->name,"backfill") and cl -> cluster_properties.find("q_backfill") != cl -> cluster_properties.end() )
+    isbackfill = true;
 
-	      return 1;
-		}
-  }
+  if (!isbackfill)
+    {
+    if (job->jinfo->queue->required_property != NULL && !is_prefix(string(job->jinfo->queue->required_property),"q_")
+        && cl -> cluster_properties.find(string(job->jinfo->queue->required_property)) == cl -> cluster_properties.end())
+      return 1;
+
+    if (strcmp(job->jinfo->queue->name, conf.default_queue) == 0 || is_prefix(job->jinfo->queue->name,"q_") || is_prefix(job->jinfo->queue->name,"backfill"))
+      for (int j = 0; j < p_info->num_queues; ++j)
+	if (p_info->queues[j]->required_property != NULL && strlen(p_info->queues[j]->required_property) > 0)
+          if (!is_prefix(p_info->queues[j]->required_property,"q_")
+              && cl -> cluster_properties.find(string(p_info->queues[j]->required_property)) != cl -> cluster_properties.end())
+	    {
+            /* pokud na clusteru tato fronta explicitne je tak to nezakazujem, muze tam*/
+            if (strcmp(job->jinfo->queue->name, p_info->queues[j]->name) == 0)
+              continue;
+
+	    return 1;
+            }
+    }
 
   const char *node_spec = strdup((char*)job -> jinfo -> nodespec.c_str());
   if ( node_spec == NULL || node_spec[0] == '\0')
@@ -457,95 +460,100 @@ int check_cluster_suitable(server_info* p_info, plan_cluster* cl, plan_job* job)
   iter = spec -> nodes -> properties;
   while (iter != NULL)
     {
-        if (iter -> value != NULL)
-        {
-          iter = iter -> next;
-          continue;
-        }
+    if (iter -> value != NULL)
+      {
+      iter = iter -> next;
+      continue;
+      }
         
-	if (iter -> name[0] == '^')
-	{
-	  if ( cl -> cluster_properties.find(string(iter -> name + 1)) != cl -> cluster_properties.end())
-	    	  {
-	    	  free_parsed_nodespec(spec);
-	    	  return 1;
-	    	  }
-	} else {
-	  //if (!is_prefix(string(iter -> name),"q_"))
-	  if ( cl -> cluster_properties.find(string(iter -> name)) == cl -> cluster_properties.end())
-    	  {
-    	  free_parsed_nodespec(spec);
-    	  return 1;
-    	  }
+    if (iter -> name[0] == '^')
+      {
+      if ( cl -> cluster_properties.find(string(iter -> name + 1)) != cl -> cluster_properties.end())
+        {
+        free_parsed_nodespec(spec);
+        return 1;
 	}
+      }
+    else
+      {
+      if ( cl -> cluster_properties.find(string(iter -> name)) == cl -> cluster_properties.end())
+        {
+        free_parsed_nodespec(spec);
+        return 1;
+        }
+      }
 
     iter = iter -> next;
     }
 
   if (job->jinfo->placement == "" and job->jinfo->placement[0] == '^')
-	  if ( cl -> cluster_properties.find(string(job->jinfo->placement.c_str() + 1)) != cl -> cluster_properties.end())
-	 	    	  {
-	 	    	  free_parsed_nodespec(spec);
-	 	    	  return 1;
-	 	    	  }
+    if ( cl -> cluster_properties.find(string(job->jinfo->placement.c_str() + 1)) != cl -> cluster_properties.end())
+      {
+      free_parsed_nodespec(spec);
+      return 1;
+      }
 
   free_parsed_nodespec(spec);
 
   req_num_nodes = job -> req_num_nodes;
 
   for (int i = 0; i < cl -> num_nodes; i++)
-	if (cl -> nodes[i] -> get_cores_total() >= job -> req_ppn)
-		req_num_nodes = req_num_nodes - (cl -> nodes[i] -> get_cores_total() / job -> req_ppn);
+    if (cl -> nodes[i] -> get_cores_total() >= job -> req_ppn)
+      req_num_nodes = req_num_nodes - (cl -> nodes[i] -> get_cores_total() / job -> req_ppn);
 
   if (req_num_nodes <= 0)
-	  return 0;
+    return 0;
   
   Resource *res;
-  if (job->req_gpu > 0) {
-      //v clusteru musi byt uzel s GPU
-      for (int i = 0; i < cl -> num_nodes; i++)
-        {
-          res = cl -> nodes[i]->get_resource("gpu");
-          if (res)
-          if (job->req_gpu <= res->get_capacity())
-              return 1;
-        }
+  if (job->req_gpu > 0)
+    {
+    //v clusteru musi byt uzel s GPU
+    for (int i = 0; i < cl -> num_nodes; i++)
+      {
+      res = cl -> nodes[i]->get_resource("gpu");
+      if (res)
+        if (job->req_gpu <= res->get_capacity())
+          return 1;
+      }
       // neni tu
-      return 0;
-     }
-      
-      
-
+     
+    return 0;
+    }
+  
   return 1;
   }
 
-long get_walltime_limit_min(std::set<std::string> properties) {
+long get_walltime_limit_min(std::set<std::string> properties)
+  {
   std::set<std::string> excluded_properties = filter_exclude(properties, 1);
   std::set<std::string>::const_iterator it;
 
   long ret = 0;
 
-  for (it = excluded_properties.begin(); it != excluded_properties.end(); it++){
-	  if (is_prefix(*it,"min_"))
-	      ret = decode_time(strdup((*it).c_str()+4));
-      }
+  for (it = excluded_properties.begin(); it != excluded_properties.end(); it++)
+    {
+    if (is_prefix(*it,"min_"))
+      ret = decode_time(strdup((*it).c_str()+4));
+    }
 
   return ret;
-}
+  }
 
-long get_walltime_limit_max(std::set<std::string> properties) {
+long get_walltime_limit_max(std::set<std::string> properties)
+  {
   std::set<std::string> excluded_properties = filter_exclude(properties, 1);
   std::set<std::string>::const_iterator it;
 
   long ret = 0;
 
-  for (it = excluded_properties.begin(); it != excluded_properties.end(); it++){
-	  if (is_prefix(*it,"max_"))
-	      ret = decode_time(strdup((*it).c_str()+4));
-      }
+  for (it = excluded_properties.begin(); it != excluded_properties.end(); it++)
+    {
+    if (is_prefix(*it,"max_"))
+      ret = decode_time(strdup((*it).c_str()+4));
+    }
 
   return ret;
-}
+  }
 
 int cluster_add_node(sched* schedule, int clus_number , node_info* node)
   {
@@ -573,10 +581,10 @@ int cluster_add_node(sched* schedule, int clus_number , node_info* node)
 
   //running cpus update
   if (! (node ->is_down() ||  node ->is_offline() || node ->is_notusable()))
-  {
-  	schedule -> clusters[clus_number] -> num_running_cpus += node -> get_cores_free();
-  	schedule -> clusters[clus_number] -> available_ram += get_node_mem(node);
-  }
+    {
+    schedule -> clusters[clus_number] -> num_running_cpus += node -> get_cores_free();
+    schedule -> clusters[clus_number] -> available_ram += get_node_mem(node);
+    }
 
   return 0;
   }
@@ -592,13 +600,6 @@ int plan_set_cluster_server(sched* schedule, int clus_number, server_info* sinfo
   schedule -> clusters[clus_number] -> sinfo = sinfo;
   schedule -> jobs[clus_number] -> sinfo = sinfo;
   schedule -> gaps[clus_number] -> sinfo = sinfo;
-  if (schedule -> clusters[clus_number]->sinfo == NULL)
-    {
-	sched_log(PBSEVENT_DEBUG2, PBS_EVENTCLASS_REQUEST, schedule -> clusters[clus_number] -> cluster_name,"WARNING! Server info deleted");
-    } else
-    {
-   // sched_log(PBSEVENT_DEBUG2, PBS_EVENTCLASS_REQUEST, "Server info in cluster updated", "cluster %d has server %s",clus_number,schedule->clusters[clus_number]->sinfo->name);
-    }
 
   return 0;
   }
@@ -618,7 +619,7 @@ int plan_find_cluster_for_node(sched* schedule,  server_info* sinfo, node_info* 
     return 1;
 
   if (ninfo->cluster_name == NULL)
-	  ninfo->cluster_name = strdup("");
+    ninfo->cluster_name = strdup("");
 
   for (int i = 0; i < schedule -> num_clusters; i++)
     if (strcmp(schedule -> clusters[i] -> cluster_name, ninfo -> cluster_name) == 0)
@@ -647,13 +648,14 @@ int plan_find_cluster_for_node(sched* schedule,  server_info* sinfo, node_info* 
   cluster_add_node(schedule, schedule -> num_clusters-1, ninfo);
   sched_log(PBSEVENT_DEBUG2, PBS_EVENTCLASS_REQUEST, "Cluster created","%s added to newly created cluster %d", ninfo -> get_name(), schedule -> num_clusters-1);
   plan_set_cluster_server(schedule, schedule -> num_clusters - 1, sinfo);
+  
   return 0;
   }
 
 int plan_update_clusters(sched* schedule,  server_info* sinfo)
   {
   for (int i = 0; i < sinfo -> num_nodes; i++)
-	plan_find_cluster_for_node(schedule, sinfo, sinfo -> nodes[i]);
+    plan_find_cluster_for_node(schedule, sinfo, sinfo -> nodes[i]);
 
   return 0;
   }
@@ -680,12 +682,14 @@ sch_resource_t job_get_scratch_local(JobInfo *jinfo)
   scratch = spec->nodes->scratch;
   
   if (spec->nodes->scratch_type == ScratchSSD)
-      scratch = -2;
-  else if (spec->nodes->scratch_type == ScratchShared) 
+    scratch = -2;
+  else 
+    if (spec->nodes->scratch_type == ScratchShared) 
       scratch = -2;
 
   free_parsed_nodespec(spec);
   free((char*) node_spec);
+  
   return scratch;
   }
 
@@ -727,7 +731,7 @@ JobInfo** plan_known_jobs_update(sched* schedule,  server_info* sinfo, time_t ti
 
       while (list_get_next(schedule -> jobs[i]) != NULL)
         if (((plan_job*)schedule -> jobs[i] -> current) -> job_id == job_id)
-        {
+          {
           found = 1;
           job = (plan_job*)schedule -> jobs[i] -> current;
 
@@ -739,9 +743,11 @@ JobInfo** plan_known_jobs_update(sched* schedule,  server_info* sinfo, time_t ti
 
           res_walltime = find_resource_req(jinfo -> resreq, "walltime");
           if (res_walltime == NULL) {
-        	  job -> estimated_processing = DEFAULT_WALLTIME;
-          } else {
-        	  job -> estimated_processing = res_walltime -> amount;
+            job -> estimated_processing = DEFAULT_WALLTIME;
+          } 
+        else 
+          {
+            job -> estimated_processing = res_walltime -> amount;
           }
 
           job -> completion_time = job -> start_time + job -> estimated_processing;
@@ -754,43 +760,45 @@ JobInfo** plan_known_jobs_update(sched* schedule,  server_info* sinfo, time_t ti
           /* hotovy job smazat z rozvrhu */
           if (jinfo -> state == JobCompleted)
             {
-        	fairshare_add_job(job, schedule -> clusters[i]->num_running_cpus, schedule -> clusters[i]->available_ram );
-         	list_remove_item(schedule -> jobs[i], schedule -> jobs[i] -> current, 1);
-         	fairshare_log();
+            fairshare_add_job(job, schedule -> clusters[i]->num_running_cpus, schedule -> clusters[i]->available_ram );
+            list_remove_item(schedule -> jobs[i], schedule -> jobs[i] -> current, 1);
+            fairshare_log();
             }
 
           //pokud je job bezici ale v tomto okamziku ma nebo uz mel koncit dame mu cas ukonceni now+1
           if ((jinfo -> state == JobRunning || jinfo -> state == JobExiting)
-             && job -> completion_time <= time_now)
-        	job -> completion_time = time_now+1;
+               && job -> completion_time <= time_now)
+            job -> completion_time = time_now+1;
 
           /* pokud budem planovat pres klastry tak se musi nasledujici radek zakomentovat */
           i = schedule -> num_clusters;
           break;
-        }
+          }
       }
 
     if (jinfo -> state == JobCompleted)
-    	found = 1;
+      found = 1;
 
     if (jinfo -> state != JobRunning && jinfo -> state != JobQueued)
-    	found = 1;
+      found = 1;
 
     // jestli je job remote tak nic
     if (jinfo -> state == JobRunning && string(jinfo->job_id).find(conf.local_server) == string::npos)
-    	found = 1;
+      found = 1;
     
     if (jinfo -> state == JobQueued && string(jinfo->job_id).find(conf.local_server) == string::npos)
       {
-        //skip remote job array
-        if (jinfo->job_id.find('-') != std::string::npos and jinfo->job_id.find('-') < jinfo->job_id.find('.'))
-          {
-            found = 1;
-          } else {
-            new_jobs_counter_remote++;
-            if (new_jobs_counter_remote > 10)
-              found = 1;    
-          }
+      //skip remote job array
+      if (jinfo->job_id.find('-') != std::string::npos and jinfo->job_id.find('-') < jinfo->job_id.find('.'))
+        {
+        found = 1;
+        }
+      else
+        {
+        new_jobs_counter_remote++;
+        if (new_jobs_counter_remote > 10)
+          found = 1;    
+        }
       }
 
     if (found == 0 && new_jobs_counter < conf.new_jobs_limit_per_cycle)
@@ -800,8 +808,6 @@ JobInfo** plan_known_jobs_update(sched* schedule,  server_info* sinfo, time_t ti
 
       if (jinfo ->state == JobQueued)
          sched_log(PBSEVENT_DEBUG2, PBS_EVENTCLASS_REQUEST, "new_queued_job", "%s %s", jinfo -> job_id.c_str(), jinfo ->account.c_str());
-
-      //jinfo->preprocess();
 
       if ((new_jobs = (JobInfo**) realloc(new_jobs, (new_jobs_counter + 1) * sizeof(JobInfo*))) == NULL)
         {
@@ -813,11 +819,10 @@ JobInfo** plan_known_jobs_update(sched* schedule,  server_info* sinfo, time_t ti
       new_jobs[new_jobs_counter] = NULL;
       }
 
-  	job_count++;
+    job_count++;
     }
 
   sched_log(PBSEVENT_DEBUG2, PBS_EVENTCLASS_REQUEST, "New jobs", " count: %d", new_jobs_counter);
 
   return new_jobs;
   }
-
